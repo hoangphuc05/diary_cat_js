f3c292af5ce9b617c096d10342544b747f371a78
Phuc Cai <chphuc05@gmail.com>
2021-12-26 00:29:54 -0800

    add confirm message and fail image upload

--- message_commands/add.js
+++ message_commands/add.js
@@ -1,5 +1,6 @@
 import {last_time as last_time_db, addEntry} from './../models/utilities.js';
 import { uploadFile } from './../utils/fileUploader.js';
+import confirm_embed from '../utils/confirm_embed.js';
 
 
 export default {
@@ -24,8 +25,14 @@ export default {
         // if (last_time){
         // if confirm is yes, or if last time is normal, continue
 
-        console.log(message.attachments);
+        //check if there's any content in the message
+        if (message_content === "" && message.attachments.size === 0){
+            message.channel.send("You need to enter some content or attach a file to post");
+            return;
+        }
+        // console.log(message.attachments);
         let streak_value;
+
         // upload the picture to AWS and get the link to it
         if (message.attachments.size > 0){
             // loop through the attachments
@@ -35,12 +42,23 @@ export default {
                 // create the entry
                 // console.log("url is", url);
                 streak_value = await addEntry(author, message_content, url, message.author.username, message.channelId);
+                if (streak_value === -1){
+                    message.channel.send("One of your picture was not added, please try again or change to another picture.")
+                }
             }
         } else {
+            // confirm with the user that they want to add entry with no picture
+            const confirm = await confirm_embed("No picture attached!", "Are you sure you want to add this entry without a picture?", message);
+            if (!confirm){
+                message.reply("No entry is added");
+                return;
+            }
             streak_value = await addEntry(author, message_content, 'None', message.author.username, message.channelId);
         }
         // console.log(message.attachments.size);
         // reply with the streak
-        message.reply(`Your current streak is ${streak_value} days!`);
+        if (streak_value !== -1){
+            message.reply(`Your current streak is ${streak_value} days!`);
+        }
     }
 }
