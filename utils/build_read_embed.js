26afe8e1f566874887fe82a21ba63a4571cd6f31
Phuc Cai <chphuc05@gmail.com>
2021-12-25 22:49:01 -0800

    add picture by presign url

---
+++ utils/build_read_embed.js
@@ -0,0 +1,33 @@
+import { MessageEmbed } from "discord.js"
+import {DateTime} from 'luxon';
+import { presignUrl } from "./presignUrl.js";
+
+export default async (entry_model) => {
+    const diary_embed = new MessageEmbed()
+        // .setTitle(`<t:${Math.floor(parseInt(entry_model.date)/1000)}:D>`)
+        // .setURL(entry_model.url)
+        .setFooter(`${entry_model.name}`)
+    
+    // check if string is unix timestamp
+    if (!entry_model.date.toString().includes('-')){
+        diary_embed.setTitle(`<t:${Math.floor(parseInt(entry_model.date)/1000)}:D>`)
+    } else {
+        diary_embed.setTitle(`<t:${Math.floor(DateTime.fromFormat(entry_model.date, "dd-MM-yyyy").toMillis()/1000)}:D>`)
+    }
+
+    // get presign url and set it as url
+    if (entry_model.url !== 'None'){
+        const presignedImageUrl = await presignUrl(entry_model.url);
+        if (presignedImageUrl){
+            diary_embed.setImage(presignedImageUrl);
+        }
+    }
+
+    // divide the message into chunks of 1023 characters
+    const message_chunks = entry_model.message.match(/.{1,1023}/g);
+    for (let i = 0; i < message_chunks.length; i++){
+        diary_embed.addField(`Note ${i+1}`, `${message_chunks[i]}`);
+    }
+
+    return diary_embed;
+}
