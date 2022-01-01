3ffa5cf7467c9043f2e2c7f7007e9c4ca11a3b49
Phuc Cai <chphuc05@gmail.com>
2022-01-01 15:34:37 -0800

    set status

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,9 @@
+// import {client} from './../index.js';
 export default {
     name: 'ready',
     once: true,
     execute(client) {
         console.log('Ready!');
+        client.user.setActivity(`dl!help or /help to get started`);
     }
 }
