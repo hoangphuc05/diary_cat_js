0c50738969a7f2ca21b6c3b3459fe9a7034cdf20
Phuc Hoang <pcai22@my.whitworth.edu>
2022-05-14 01:27:27 -0700

    deprecation notice on addtext

--- message_commands/addtext.js
+++ message_commands/addtext.js
@@ -1,13 +1,20 @@
 import {last_time as last_time_db, addEntry} from './../models/utilities.js';
 import { uploadFile } from './../utils/fileUploader.js';
 import confirm_embed from '../utils/confirm_embed.js';
-
+import { MessageEmbed } from "discord.js";
 
 export default {
     name: 'addtext',
     description: 'Add a new entries',
     aliases: ['addtextanyway'],
     async execute(message, args) {
+
+        // send deprecation notice
+        const embed = new MessageEmbed()
+            .setTitle("Deprecation Notice")
+            .setDescription("This command is going to be deprecated, please use the [slash command](https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ) `addtext` instead");
+
+        message.channel.send({embeds: [embed]});
         
         //send typing indicator
         message.channel.sendTyping();
